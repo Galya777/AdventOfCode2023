5cea4f3e67b0d328aaa1017583288c8d738020af
Galya <58034004+Galya777@users.noreply.github.com>
2023-12-04 09:14:50 +0100

    Update Day1_Java.java

--- Day1_Java.java
+++ Day1_Java.java
@@ -1,30 +1,30 @@
 public class hey {
 
-     public static  char[][] read() throws IOException {
-        BufferedReader bo = new BufferedReader(new("input.txt"));
+    public static char[][] read() throws IOException {
+        BufferedReader bo = new BufferedReader(new FileReader("input.txt"));
         int column = Integer.parseInt(bo.readLine());
         int row = Integer.parseInt(bo.readLine());
-        char[][] map = new String[row][column];
+        char[][] map = new char[row][column];
         for (int i = 0; i < row; i++) {
             String line = bo.readLine();
             for (int j = 0; j < column; j++) {
-                map[i][j] = String.valueOf(line.charAt(j));
+                map[i][j] = line.charAt(j);
             }
-        } 
+        }
         bo.close();
         return map;
     }
 }
 class HelloWorld {
     public static void main(String[] args) {
-       char[][] data=read();
+       char[][] data=hey.read();
        int sum=0;
        int num=0;
-       for(int i=0; i<data.size(); ++i){
-      for(int j=0; j<data[i].size(); ++j){
+       for(int i=0; i<data.length; ++i){
+      for(int j=0; j<data[i].length; ++j){
           int counter=1;
            if(isDigit(data[i][j])){
-               num=data[i][j]+10*counter;
+               num= Character.getNumericValue(data[i][j])+10*counter;
                --counter;
            }
        }
